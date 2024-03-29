4c1068b81a21f7c63039b1e59a26e91ff2197d24
Ethan Holz <ethan.holz@thoriumworks.com>
2024-03-29 17:38:08 -0500

    refactor: move to using vim.loop over jobstart

--- lua/freeze/init.lua
+++ lua/freeze/init.lua
@@ -1,21 +1,53 @@
+local loop = vim.loop
 local freeze = {}
+local output = { stdout = "", stderr = "" }
+
+local function onReadStdOut(err, data)
+	if err then
+		vim.notify(err, vim.log.levels.ERROR, { title = "Freeze" })
+	end
+	if data then
+		output.stdout = output.stdout .. data
+	end
+end
+
+local function onReadStdErr(err, data)
+	if err then
+		vim.notify(err, vim.log.levels.ERROR, { title = "Freeze" })
+	end
+	if data then
+		output.stderr = output.stderr .. data
+	end
+end
+
+local function onExit(stdout, stderr)
+	return vim.schedule_wrap(function(code, _)
+		if code == 0 then
+			vim.notify("Successfull frozen 🍦", vim.log.levels.INFO, { title = "Freeze" })
+		else
+			vim.notify(output.stdout, vim.log.levels.ERROR, { title = "Freeze" })
+		end
+		stdout:read_stop()
+		stderr:read_stop()
+		stdout:close()
+		stderr:close()
+	end)
+end
 
 function freeze.freeze(start_line, end_line)
-	local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
-	-- Escape our input for the shell
-	local input = table.concat(lines, "\n"):gsub('"', '\\"')
 	local language = vim.api.nvim_buf_get_option(0, "filetype")
-	local command = string.format('echo "%s" | freeze --language %s', input, language)
-	local job_opts = {
-		on_exit = function(_, code)
-			if code == 0 then
-				vim.notify("Successfully frozen 🍦", vim.log.levels.INFO, { title = "Freeze" })
-			else
-				vim.notify("Failed to freeze", vim.log.levels.ERROR, { title = "Freeze" })
-			end
-		end,
-	}
-	vim.fn.jobstart(command, job_opts)
+	local file = vim.api.nvim_buf_get_name(0)
+	local stdout = loop.new_pipe(false)
+	local stderr = loop.new_pipe(false)
+	local handle = loop.spawn("freeze", {
+		args = { "--language", language, "--lines", start_line .. "," .. end_line, file },
+		stdio = { nil, stdout, stderr },
+	}, onExit(stdout, stderr))
+	if not handle then
+		vim.notify("Failed to spawn freeze", vim.log.levels.ERROR, { title = "Freeze" })
+	end
+	loop.read_start(stdout, onReadStdOut)
+	loop.read_start(stderr, onReadStdErr)
 end
 
 function freeze.setup()
